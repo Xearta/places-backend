45a2fafa7d2fe9b738d14d433cf3801ff83fb76c
Xearta <rsmcwow@gmail.com>
2020-11-22 12:56:39 -0500

    Created a route for places/:pid + places/user/:uid

---
+++ routes/users-routes.js
@@ -0,0 +1,24 @@
+// const express = require('express');
+
+// const router = express.Router();
+
+// const USERS = [
+//   {
+//     id: 'u1',
+//     name: 'Tanner Townsend',
+//     image:
+//       'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.proheadshots.ca%2Fwp-content%2Fuploads%2F2016%2F02%2Fdoctors-headshot.jpg&f=1&nofb=1',
+//     places: 3,
+//   },
+// ];
+
+// router.get('/', (req, res, next) => {
+//   const userId = req.params.pid; // { pid: 'u1' }
+//   const user = USERS.find(u => {
+//     return u.id === userId;
+//   });
+
+//   res.json({ user });
+// });
+
+// module.exports = router;
