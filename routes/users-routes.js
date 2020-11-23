800bb8279fa53ff84bacd0e914287c65adb0d911
Xearta <rsmcwow@gmail.com>
2020-11-23 10:30:12 -0500

    Created user routes for GET all, signup, and login

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,24 +1,13 @@
-// const express = require('express');
+const express = require('express');
 
-// const router = express.Router();
+const usersController = require('../controllers/users-controller');
 
-// const USERS = [
-//   {
-//     id: 'u1',
-//     name: 'Tanner Townsend',
-//     image:
-//       'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.proheadshots.ca%2Fwp-content%2Fuploads%2F2016%2F02%2Fdoctors-headshot.jpg&f=1&nofb=1',
-//     places: 3,
-//   },
-// ];
+const router = express.Router();
 
-// router.get('/', (req, res, next) => {
-//   const userId = req.params.pid; // { pid: 'u1' }
-//   const user = USERS.find(u => {
-//     return u.id === userId;
-//   });
+router.get('/', usersController.getUsers);
 
-//   res.json({ user });
-// });
+router.post('/signup', usersController.signup);
 
-// module.exports = router;
+router.post('/login', usersController.login);
+
+module.exports = router;
